80d56aa01bc4b7fdddeaceefd46d25304b643932
vmastr <143725645+vmastr@users.noreply.github.com>
2024-09-30 09:03:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="magicMahler",               # Name of your package
-    version="1.0",                  # Version of your package
-    packages=find_packages(),         # Automatically find packages
-    install_requires=[],              # List of dependencies (if any)
-    include_package_data=True,        # Include package data specified in MANIFEST.in
-    description="A package for polytopes and related computations.",  # Short description
-    author="vmastr",               # Your name
-    author_email="",  # Your email
-    url="https://github.com/yourusername/magicMahler",  # Optional: URL to your project's homepage or repo
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name="magicMahler",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "tqdm",
+        "imageio"
     ],
+    description="A Python package for working with polytopes, polar bodies, and Mahler volumes.",
+    author="Vlassis Mastrantonis",
+    author_email="vmastr@umd.edu",
+    url="https://github.com/vmastr/magicMahler",
 )
